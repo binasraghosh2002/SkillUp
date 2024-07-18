0db48cb2ce17af3460f571c98a6405e00403ab21
binasraghosh2002 <binasraghosh2002@gmail.com>
2024-07-18 14:42:19 +0530

    first commit

---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          <div className='row'>
+            <Heading subtitle='WELCOME TO SKILL UP' title='Master essential skills and unlock your potential.' />
+            <p>Your go-to e-learning platform for mastering essential skills and unlocking your potential.</p>
+            <div className='button'>
+              <button className='primary-btn'>
+                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
